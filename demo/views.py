ebfd579f29ee055fbea5acbf04235d60b31c833d
RifaBintee <115500441+RifaBintee@users.noreply.github.com>
2023-05-01 02:57:18 +0600

    views.py(registration, log in ,log out ,passwordchange)

--- demo/views.py
+++ demo/views.py
@@ -26,3 +26,63 @@ def index(request):
 @login_required(login_url='login')
 def home(request):
     return render(request, 'home.html')
+
+
+#sign-up er kaj korsi
+def registration(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserFrom()
+
+        if request.method == 'POST': #form theke data netese
+            form = CreateUserFrom(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for '+ user)
+                return redirect('login')
+
+        context = {'form': form}
+        return render(request, 'registration.html', context)
+
+#login page er kaj korsi
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth_login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request,'Username or Password is incorrect')
+
+
+        context = {}
+        return render(request, 'login.html', context) #login hobe
+
+
+#logout er kaj korsi
+def logoutUser(request):
+    logout(request)
+    return redirect('index')
+
+
+#password Update er kaj korsi
+class PasswordsChangeView(PasswordChangeView):
+
+    form_class = PasswordChangingForm
+
+    success_url = reverse_lazy('view-profile')
+
+
+# dorkar nai ei page.ei page er kaj holo password change korle ekta success msg dey
+# def password_Success(request):
+#     return render(request,'password_success.html', {})
