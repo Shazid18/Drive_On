dfe8b90c9836f2dcbba95435a5c4db09252e9c33
rahatkhanovik <20101015@uap-bd.edu>
2023-05-01 03:44:39 +0600

    rentInfo models.py updated.

--- rentInfo/models.py
+++ rentInfo/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+from carInfo.models import Rental_Car
+
+
 # Create your models here.
+
+class Rent_Car_Record(models.Model):
+    rent_car_id = models.ForeignKey(Rental_Car, on_delete=models.DO_NOTHING)
+    user_profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    rent_time = models.DateTimeField()
+    return_time = models.DateTimeField()
+    pickup_loc = models.CharField(max_length=100)
