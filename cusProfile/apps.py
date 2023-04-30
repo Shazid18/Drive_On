2b23d84276a3c1ac39d00995f9375512cfbe6098
RifaBintee <115500441+RifaBintee@users.noreply.github.com>
2023-05-01 04:04:52 +0600

    updated cusprofile apps.py

--- cusProfile/apps.py
+++ cusProfile/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CusprofileConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cusProfile'
+
+    def ready(self):
+        import cusProfile.signals
