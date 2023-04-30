e3c86a3ef0fad7f3ab28e741896addb6aa5603f9
Shazid18 <exvalo69@gmail.com>
2023-05-01 03:35:12 +0600

    update admin.py for selling car, rental car, driver ditels database shown

--- carInfo/admin.py
+++ carInfo/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+@admin.register(Selling_Car)
+class SellCarDetailsAdmin(admin.ModelAdmin):
+    list_display = ['id','model','type','price','shop_name']
+
+@admin.register(Rental_Car)
+class RentCarDetailsAdmin(admin.ModelAdmin):
+    list_display = ['id','car_model','car_type','renter_name']
+
+@admin.register(Driver)
+class DriverDetailsAdmin(admin.ModelAdmin):
+    list_display = ['id','dri_name','dri_lic_num']
