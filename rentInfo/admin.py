299e9b2dc084aab4704c559e712571cfd6b4dc9a
rahatkhanovik <20101015@uap-bd.edu>
2023-05-01 03:46:39 +0600

    rentInfo admin.py updated.

--- rentInfo/admin.py
+++ rentInfo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+@admin.register(Rent_Car_Record)
+class RentCarRecordDetailsAdmin(admin.ModelAdmin):
+    list_display = ['id','rent_car_id','user_profile','rent_time','return_time']
