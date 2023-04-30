5f9ede3fbad16910392f90156271c76955770c80
T-shawon <toufiq.shawon404@gmail.com>
2023-05-01 03:54:00 +0600

    updated payment app

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+@admin.register(Sell_Car_Payment)
+class SellCarPaymentDetailsAdmin(admin.ModelAdmin):
+    list_display = ['id','sell_car_id','user_profile','transition_id']
+
+@admin.register(Driver_Payment)
+class DriverPaymentDetailsAdmin(admin.ModelAdmin):
+    list_display = ['id','driver_id','user_profile','transition_id']
+
+@admin.register(Rental_Car_payment)
+class RentalCarPaymentDetailsAdmin(admin.ModelAdmin):
+    list_display = ['id','rental_car_id','user_profile_id','transition_id']
--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+from carInfo.models import Rental_Car, Selling_Car, Driver
+
+
 # Create your models here.
+
+
+class Sell_Car_Payment(models.Model):
+    sell_car_id = models.ForeignKey(Selling_Car, on_delete=models.DO_NOTHING)
+    user_profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    transition_id = models.CharField(max_length=15)
+
+class Driver_Payment(models.Model):
+    driver_id = models.ForeignKey(Driver, on_delete=models.DO_NOTHING)
+    user_profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    transition_id = models.CharField(max_length=15)
+
+class Rental_Car_payment(models.Model):
+    rental_car_id = models.CharField(max_length=5)
+    user_profile_id = models.CharField(max_length=5)
+    transition_id = models.CharField(max_length=15)
