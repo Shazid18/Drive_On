89bcef8d71aaf4b1abbd7a8b8feddfbb69ddb1e4
Shazid18 <exvalo69@gmail.com>
2023-05-01 04:12:40 +0600

    update Profile's models.py

--- Profile/models.py
+++ Profile/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    user_name = models.CharField(max_length=20)
+    email_add = models.EmailField()
+    phone_num = models.CharField(max_length=11)
+    birth_date = models.DateField()
+    credit_card_num = models.CharField(max_length=10)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=20)
+    zip_code = models.IntegerField()
+    profile_pic = models.ImageField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
