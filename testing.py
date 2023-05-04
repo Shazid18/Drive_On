4d4c0890a1731d4294ba7c49261744ce333ee10a
Shazid18 <exvalo69@gmail.com>
2023-05-05 03:19:44 +0600

    Testing setup

---
+++ testing.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome(executable_path="E:\\UAP 3.2\\CSE 322\\Selenium\\chromedriver.exe")
+driver.maximize_window()
+
