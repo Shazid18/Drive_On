b7ba915bbaebf072ec124e61757d43c5e9cb4386
rahatkhanovik <20101015@uap-bd.edu>
2023-05-05 20:53:31 +0600

    Test case 1 update

--- testreport.py
+++ testreport.py
@@ -11,3 +11,19 @@ report = Report()
 report.setup(report_folder=r"E:\UAP 3.2\SSS\DriveOn\TestingReport", module_name="Report", release_name="release 1", selenium_driver=driver)
 
 
+# Test Case 1
+try:
+    report.write_step("Goto Index Page", status=report.status.Start, test_number=1)
+
+    driver.get("http://127.0.0.1:8000")
+    time.sleep(2)
+    assert (driver.title == 'Home:DriveOn')
+    report.write_step('Index Page loaded Successfully.', status=report.status.Pass, screenshot=True)
+
+except AssertionError:
+    report.write_step('Index Page loaded Failed.', status=report.status.Fail, screenshot=True)
+
+except Exception as e:
+    report.write_step('Something went wrong!!!', status=report.status.Warn, screenshot=True)
+
+
